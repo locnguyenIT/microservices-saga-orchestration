e84a4bdbdefe2c3fb8dab25a1788f0e506a45fb7
APAC\AOY1HC <loc.nguyenthanh2@vn.bosch.com>
2023-04-11 18:54:18 +0700

    add order view service

---
+++ order-view-service/src/main/java/com/ntloc/ordeview/OrderViewApplication.java
@@ -0,0 +1,11 @@
+package com.ntloc.ordeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderViewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderViewApplication.class, args);
+    }
+}
