5745cc54c3a1336587b809453d53d00f6e5d0059
APAC\AOY1HC <loc.nguyenthanh2@vn.bosch.com>
2023-04-14 19:39:20 +0700

    handler event from difference service

--- payment-service/src/main/java/com/ntloc/payment/handler/PaymentHandler.java
+++ payment-service/src/main/java/com/ntloc/payment/handler/PaymentHandler.java
@@ -1,12 +1,16 @@
 package com.ntloc.payment.handler;
 
+import com.ntloc.coreapi.delivery.event.OrderDeliveredEvent;
+import com.ntloc.coreapi.order.event.OrderCompletedEvent;
 import com.ntloc.coreapi.order.event.OrderCreatedEvent;
 import com.ntloc.coreapi.payment.event.PaymentSucceededEvent;
 import com.ntloc.payment.Payment;
 import com.ntloc.payment.PaymentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class PaymentHandler {
 
@@ -20,5 +24,12 @@ public class PaymentHandler {
     public void on(PaymentSucceededEvent event) {
         Payment order = new Payment(event.paymentId(), event.orderId());
         paymentRepository.save(order);
+    }
+
+    @EventHandler
+    public void on(OrderCompletedEvent event) {
+        log.info("Payment handler  OrderCompletedEvent : {}",event);
+//        Payment order = new Payment(event.paymentId(), event.orderId());
+//        paymentRepository.save(order);
     }
 }
